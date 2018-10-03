7274095b2a034babfa48a055af055c6dcc2780cd
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2018-10-03 13:42:34 +0200

    ændret lidt på orden. avg og sum implementeret.

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -8,21 +8,7 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
-        public static double Maximum(double[] doubleArray)
-        {
-            int i;
-            double maxDouble;
-            maxDouble = doubleArray[0];
-            for (i = 0; i < doubleArray.Length; i = i + 1)
-            {
-                if (doubleArray[i] > maxDouble)
-                {
-                    maxDouble = doubleArray[i];
-                }
-            }
-            return maxDouble;
-        }
-
+        
         public static double Add(double v1, double v2)
         {
             throw new NotImplementedException();
@@ -32,40 +18,67 @@ namespace CalculatorLibrary
         {
             throw new NotImplementedException();
         }
-
-        public static double Minimum(double[] numbers)
-        {
-            int i;
-            double minInts;
-                minInts = numbers[0];
-                for (i = 0; i < numbers.Length; i = i + 1)
-                {
-                    if (numbers[i] < minInts)
-                    {
-                        minInts = numbers[i];
-                    }
-                }
-                return minInts;
-        }
-
-        public static double Divide(double v1, double v2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static double Sum(double[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
         public static double Multiply(double v1, double v2)
         {
             throw new NotImplementedException();
         }
-
-        public static double Average(double[] numbers)
+        public static double Divide(double v1, double v2)
         {
             throw new NotImplementedException();
+        }
+        public static double Sum(double[] numbers)
+        {
+            int i;
+            double sumNumbers;
+            sumNumbers = 0;
+            for (i = 0; i < numbers.Length; i++)
+            {
+                // Kan ændres til sumInts += ints[i], men det kan Allan ikke lide,
+                // så det kan vi heller ikke lide.
+                // han kan heller ikke rigtigt lide kommentarer, så vi må nok hellere slette det her
+                sumNumbers = sumNumbers + numbers[i];
+            }
+            return sumNumbers;
+        }
+        public static double Minimum(double[] numbers)
+        {
+            int i;
+            double minInts;
+            minInts = numbers[0];
+            for (i = 0; i < numbers.Length; i = i + 1)
+            {
+                if (numbers[i] < minInts)
+                {
+                    minInts = numbers[i];
+                }
+            }
+            return minInts;
+        }
+        public static double Maximum(double[] numbers)
+        {
+            int i;
+            double maxNumbers;
+            maxNumbers = numbers[0];
+            for (i = 0; i < numbers.Length; i = i + 1)
+            {
+                if (numbers[i] > maxNumbers)
+                {
+                    maxNumbers = numbers[i];
+                }
+            }
+            return maxNumbers;
+        }
+        public static double Average(double[] numbers)
+        {
+            int i;
+            double avgNumbers;
+            avgNumbers = 0;
+            for (i = 0; i < numbers.Length; i++)
+            {
+                avgNumbers = avgNumbers + numbers[i];
+            }
+            avgNumbers = avgNumbers / numbers.Length;
+            return avgNumbers;
         }
     }
 }
